1f5ddfe2f009976ffb512de8b743017f8a1c134a
ahmedazim7804 <ahmedazim7804@gmail.com>
2023-12-21 21:31:53 +0530

    changed all-time-stats.json file structure

--- save.py
+++ save.py
@@ -23,11 +23,15 @@ def save():
     json_data['pfp'] = parse_other_data.profile_picture()
     json_data['username'] = username
 
+    trakt = {}
+    trakt['most_watched_movies'] = parse_list_data.trakt_most_watched_movies()
+    trakt['most_watched_shows'] = parse_list_data.trakt_most_watched_shows()
+    json_data['trakt'] = trakt
+
     tv = {}
     tv['stats'] = parse_tv_data.tv_stats()
     tv['plays'] = parse_tv_data.plays_by_time()
     tv['users_top_10'] = parse_tv_data.users_top_10_watched_shows()
-    tv['most_watched_shows'] = parse_list_data.trakt_most_watched_shows()
     tv['by_genre'] = parse_tv_data.shows_by_genre()
     tv['by_released_year'] = parse_tv_data.shows_by_released_year()
     tv['by_country'] = parse_tv_data.shows_by_country()
@@ -40,7 +44,6 @@ def save():
     movies['stats'] = parse_movie_data.movie_stats()
     movies['plays'] = parse_movie_data.plays_by_time()
     movies['users_top_10'] = parse_movie_data.users_top_10_watched_movies()
-    movies['most_watched_movies'] = parse_list_data.trakt_most_watched_movies()
     movies['by_genre'] = parse_movie_data.movies_by_genre()
     movies['by_released_year'] = parse_movie_data.movies_by_released_year()
     movies['by_country'] = parse_movie_data.movies_by_country()
@@ -54,5 +57,5 @@ def save():
     json_data['directors'] = parse_crew.most_watched_directors()
     json_data['writers'] = parse_crew.most_watched_writers()
 
-    with open('trakt-all-time-stats.json', 'w') as file:
+    with open('all-time-stats.json', 'w') as file:
         json.dump(json_data, file, indent=4)
